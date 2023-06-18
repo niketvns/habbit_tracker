e9acde2c1bdb691ff3b37dc18e1da2a39ca8b4ea
NIKET KUMAR MISHRA <niketvns97@gmail.com>
2023-06-18 18:14:33 +0530

    tailwind-vite-setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="app-main">
+
+      </div>
+    </>
+  )
+}
+
+export default App
